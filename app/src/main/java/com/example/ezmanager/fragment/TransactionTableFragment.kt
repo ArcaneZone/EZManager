78db9a2e66267a12b60200cd4cbd41b58e0107e7
arcaneop <siddhianilbairagi@gmail.com>
2021-09-17 19:52:43 +0530

    improved UI as well as implemented transaction type

--- app/src/main/java/com/example/ezmanager/fragment/TransactionTableFragment.kt
+++ app/src/main/java/com/example/ezmanager/fragment/TransactionTableFragment.kt
@@ -9,11 +9,19 @@ import android.view.ViewGroup
 import android.widget.TableLayout
 import android.widget.TextView
 import android.graphics.Color
+import android.graphics.Color.*
+import android.provider.CalendarContract
+import android.view.Gravity
 import android.widget.TableRow
+import androidx.annotation.ColorInt
+import com.example.ezmanager.R
+import com.example.ezmanager.database.DatabaseHandler
+import com.example.ezmanager.model.Transaction
 
 
 class TransactionTableFragment : Fragment() {
      lateinit var tableLayout: TableLayout
+     lateinit var tableLayoutFinal: TableLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,74 +34,58 @@ class TransactionTableFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view=inflater.inflate(com.example.ezmanager.R.layout.fragment_transaction_table, container, false)
+        val db=DatabaseHandler(requireContext())
+        val trasactionList=db.viewTransactions()
 
         tableLayout=view.findViewById(com.example.ezmanager.R.id.table_main)
-        val tbrow0 = TableRow(context)
-        val tv0 = TextView(context)
-        tv0.text = "Sl.No"
-        tv0.setTextColor(Color.WHITE)
-        tbrow0.addView(tv0)
-        val tv1 = TextView(context)
-        tv1.text = " Title "
-        tv1.setTextColor(Color.WHITE)
-        tbrow0.addView(tv1)
-        val tv2 = TextView(context)
-        tv2.text = "Amount"
-        tv2.setTextColor(Color.WHITE)
-        tbrow0.addView(tv2)
-        val tv3 = TextView(context)
-        tableLayout.addView(tbrow0)
+        tableLayoutFinal=view.findViewById(R.id.table_result)
+        val itr=trasactionList.listIterator()
+        var i=1
+        while (itr.hasNext()) {
+            val tbrow = TableRow(context)
+
+            val transaction:Transaction=itr.next()
+
+            val t1v = TextView(context)
+            t1v.text = (i++).toString()
+            t1v.gravity = Gravity.CENTER
+            tbrow.addView(t1v)
+            val t2v = TextView(context)
+            t2v.text = transaction.title
+            t2v.gravity = Gravity.CENTER
+            tbrow.addView(t2v)
+            val t3v = TextView(context)
+            t3v.text = transaction.amount.toString()
+            t3v.gravity = Gravity.CENTER
+            tbrow.addView(t3v)
+            val t4v = TextView(context)
+            t4v.text=transaction.date
+            t4v.gravity=Gravity.CENTER
+            tbrow.addView(t4v)
+            val t5v = TextView(context)
+            if (transaction.type=="C")
+                t5v.text = "Credited"
+            else
+                t5v.text="Debited"
+            t5v.gravity = Gravity.CENTER
+            tbrow.addView(t5v)
+            tableLayout.addView(tbrow)
+
+        }
+        val tbRowFinal = TableRow(context)
+        val fT1v=TextView(context)
+        fT1v.text="Total Transaction Amount:"
+        fT1v.gravity=Gravity.CENTER
+        tbRowFinal.addView(fT1v)
+
+        val fT2v=TextView(context)
+        fT2v.text=db.sumTransaction().toString()
+        fT2v.gravity=Gravity.LEFT
+        tbRowFinal.addView(fT2v)
+        tableLayoutFinal.addView(tbRowFinal)
 
 
-        
 
         return view
     }
-    /*fun init() {
-        val stk = findViewById(R.id.table_main) as TableLayout
-        val tbrow0 = TableRow(this)
-        val tv0 = TextView(this)
-        tv0.text = " Sl.No "
-        tv0.setTextColor(Color.WHITE)
-        tbrow0.addView(tv0)
-        val tv1 = TextView(this)
-        tv1.text = " Product "
-        tv1.setTextColor(Color.WHITE)
-        tbrow0.addView(tv1)
-        val tv2 = TextView(this)
-        tv2.text = " Unit Price "
-        tv2.setTextColor(Color.WHITE)
-        tbrow0.addView(tv2)
-        val tv3 = TextView(this)
-        tv3.text = " Stock Remaining "
-        tv3.setTextColor(Color.WHITE)
-        tbrow0.addView(tv3)
-        stk.addView(tbrow0)
-        for (i in 0..24) {
-            val tbrow = TableRow(this)
-            val t1v = TextView(this)
-            t1v.text = "" + i
-            t1v.setTextColor(Color.WHITE)
-            t1v.gravity = Gravity.CENTER
-            tbrow.addView(t1v)
-            val t2v = TextView(this)
-            t2v.text = "Product $i"
-            t2v.setTextColor(Color.WHITE)
-            t2v.gravity = Gravity.CENTER
-            tbrow.addView(t2v)
-            val t3v = TextView(this)
-            t3v.text = "Rs.$i"
-            t3v.setTextColor(Color.WHITE)
-            t3v.gravity = Gravity.CENTER
-            tbrow.addView(t3v)
-            val t4v = TextView(this)
-            t4v.text = "" + i * 15 / 32 * 10
-            t4v.setTextColor(Color.WHITE)
-            t4v.gravity = Gravity.CENTER
-            tbrow.addView(t4v)
-            stk.addView(tbrow)
-        }
-    }*/
-
-
 }
